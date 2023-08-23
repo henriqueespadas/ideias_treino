e4d07b6f1f300c174ebd5826d30b6754b58ef05e
henriqueespadas <henriqueep@hotmail.com>
2023-08-23 20:06:01 -0300

    Atribuindo Player a um usuário.

--- custom_addons/magnata_global/models/mg_player.py
+++ custom_addons/magnata_global/models/mg_player.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class MagnataGlobalPlayer(models.Model):
@@ -15,3 +16,18 @@ class MagnataGlobalPlayer(models.Model):
     portfolio_ids = fields.One2many(
         "magnata.global.portfolio", "player_id", string="Portfólio"
     )
+    user_id = fields.Many2one('res.users', string='User', required=True, ondelete='cascade', index=True)
+
+    @api.constrains('user_id')
+    def _check_unique_user_id(self):
+        for record in self:
+            if self.search_count([('user_id', '=', record.user_id.id)]) > 1:
+                raise ValidationError('Um usuário só pode ter um jogador associado.')
+
+    @api.model
+    def create(self, vals):
+        vals['user_id'] = self.env.user.id
+        return super(MagnataGlobalPlayer, self).create(vals)
+
+
+
